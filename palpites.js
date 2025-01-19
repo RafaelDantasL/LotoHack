332516b5ab6d4447a2025600b003d7a37c30dd63
Rafael <157317208+RafaelDantasL@users.noreply.github.com>
2025-01-19 20:30:51 -0300

    Atualização automática dos palpites em 2025-01-19

--- palpites.js
+++ palpites.js
@@ -273,40 +273,5 @@ const palpites = {
       "Resultado do Jogo do Bicho SP, 20hs - PTN SP, 1º ao 10º\nDeu Dezena, Palpite 6974 no 10º Prêmio!",
       "Resultado do Jogo do Bicho SP, 20hs - PTN SP, 1º ao 10º\nDeu Dezena, Palpite 6274 no 10º Prêmio!"
     ]
-  },
-  "LOTECE": {
-    "palpites": [
-      "0311",
-      "0319",
-      "0312",
-      "0361",
-      "0369",
-      "0362",
-      "0381",
-      "0389",
-      "0382",
-      "0711",
-      "0719",
-      "0712",
-      "0761",
-      "0769",
-      "0762",
-      "0781",
-      "0789",
-      "0782",
-      "0911",
-      "0919",
-      "0912",
-      "0961",
-      "0969",
-      "0962",
-      "0981",
-      "0989",
-      "0982",
-      "2311",
-      "2319",
-      "2312"
-    ],
-    "frases": []
   }
 };
