c62f7261026cb7075bfd89d5fa1bf09a3dc1517d
Rafael <157317208+RafaelDantasL@users.noreply.github.com>
2025-01-29 10:32:07 -0300

    Atualização automática dos palpites em 2025-01-29

--- palpites.js
+++ palpites.js
@@ -1,4 +1,44 @@
 const palpites = {
+  "BAHIA MALUCA": {
+    "palpites": [
+      "5051",
+      "5053",
+      "5059",
+      "5001",
+      "5003",
+      "5009",
+      "5011",
+      "5013",
+      "5019",
+      "5151",
+      "5153",
+      "5159",
+      "5101",
+      "5103",
+      "5109",
+      "5111",
+      "5113",
+      "5119",
+      "5251",
+      "5253",
+      "5259",
+      "5201",
+      "5203",
+      "5209",
+      "5211",
+      "5213",
+      "5219",
+      "0051",
+      "0053",
+      "0059"
+    ],
+    "frases": [
+      "10h - MALUCA - Resultado do dia 29/01/2025 (Quarta-feira)\nDeu Dezena, Palpite 3143 no 8º Prêmio!",
+      "10h - MALUCA - Resultado do dia 29/01/2025 (Quarta-feira)\nDeu Dezena, Palpite 3443 no 8º Prêmio!",
+      "10h - MALUCA - Resultado do dia 29/01/2025 (Quarta-feira)\nDeu Dezena, Palpite 3643 no 8º Prêmio!",
+      "10h - MALUCA - Resultado do dia 29/01/2025 (Quarta-feira)\nDeu Dezena, Palpite 1143 no 8º Prêmio!"
+    ]
+  },
   "BAHIA": {
     "palpites": [
       "1370",
@@ -38,41 +78,6 @@ const palpites = {
       "10h - BA - Resultado do dia 29/01/2025 (Quarta-feira)\nDeu Dezena, Palpite 4110 no 10º Prêmio!",
       "10h - BA - Resultado do dia 29/01/2025 (Quarta-feira)\nDeu Dezena, Palpite 4810 no 10º Prêmio!"
     ]
-  },
-  "BAHIA MALUCA": {
-    "palpites": [
-      "3140",
-      "3143",
-      "3148",
-      "3160",
-      "3163",
-      "3168",
-      "3100",
-      "3103",
-      "3108",
-      "3440",
-      "3443",
-      "3448",
-      "3460",
-      "3463",
-      "3468",
-      "3400",
-      "3403",
-      "3408",
-      "3640",
-      "3643",
-      "3648",
-      "3660",
-      "3663",
-      "3668",
-      "3600",
-      "3603",
-      "3608",
-      "1140",
-      "1143",
-      "1148"
-    ],
-    "frases": []
   },
   "LOTECE": {
     "palpites": [
