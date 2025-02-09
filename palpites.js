6126906183ee97635ce19ebe9acd0cfec26a6170
Rafael <157317208+RafaelDantasL@users.noreply.github.com>
2025-02-09 10:32:11 -0300

    Atualização automática dos palpites em 2025-02-09

--- palpites.js
+++ palpites.js
@@ -1,4 +1,44 @@
 const palpites = {
+  "BAHIA MALUCA": {
+    "palpites": [
+      "8331",
+      "8338",
+      "8330",
+      "8311",
+      "8318",
+      "8310",
+      "8321",
+      "8328",
+      "8320",
+      "8531",
+      "8538",
+      "8530",
+      "8511",
+      "8518",
+      "8510",
+      "8521",
+      "8528",
+      "8520",
+      "8731",
+      "8738",
+      "8730",
+      "8711",
+      "8718",
+      "8710",
+      "8721",
+      "8728",
+      "8720",
+      "4331",
+      "4338",
+      "4330"
+    ],
+    "frases": [
+      "10h - MALUCA - Resultado do dia 09/02/2025 (Domingo)\nDeu Dezena, Palpite 5129 no 4º Prêmio!",
+      "10h - MALUCA - Resultado do dia 09/02/2025 (Domingo)\nDeu Dezena, Palpite 5229 no 4º Prêmio!",
+      "10h - MALUCA - Resultado do dia 09/02/2025 (Domingo)\nDeu Dezena, Palpite 5029 no 4º Prêmio!",
+      "10h - MALUCA - Resultado do dia 09/02/2025 (Domingo)\nDeu Dezena, Palpite 3129 no 4º Prêmio!"
+    ]
+  },
   "BAHIA": {
     "palpites": [
       "5135",
@@ -37,41 +77,6 @@ const palpites = {
       "10h - BA - Resultado do dia 09/02/2025 (Domingo)\nDeu Dezena, Palpite 0968 no 6º Prêmio!",
       "10h - BA - Resultado do dia 09/02/2025 (Domingo)\nDeu Dezena, Palpite 0068 no 6º Prêmio!"
     ]
-  },
-  "BAHIA MALUCA": {
-    "palpites": [
-      "5120",
-      "5123",
-      "5129",
-      "5150",
-      "5153",
-      "5159",
-      "5110",
-      "5113",
-      "5119",
-      "5220",
-      "5223",
-      "5229",
-      "5250",
-      "5253",
-      "5259",
-      "5210",
-      "5213",
-      "5219",
-      "5020",
-      "5023",
-      "5029",
-      "5050",
-      "5053",
-      "5059",
-      "5010",
-      "5013",
-      "5019",
-      "3120",
-      "3123",
-      "3129"
-    ],
-    "frases": []
   },
   "LOTECE": {
     "palpites": [
