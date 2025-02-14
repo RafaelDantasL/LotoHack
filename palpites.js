25c7340e98e344ad3e8a3f39f5ff21a924c0f4ae
Rafael <157317208+RafaelDantasL@users.noreply.github.com>
2025-02-14 10:56:12 -0300

    Atualização automática dos palpites em 2025-02-14

--- palpites.js
+++ palpites.js
@@ -114,6 +114,41 @@ const palpites = {
       "Resultado do Jogo do Bicho LOTECE - CE, 11:00 (manhã)\nDeu Dezena, Palpite 6935 no 2º Prêmio!"
     ]
   },
+  "LOOK/GOIAS": {
+    "palpites": [
+      "0443",
+      "0444",
+      "0448",
+      "0463",
+      "0464",
+      "0468",
+      "0423",
+      "0424",
+      "0428",
+      "0643",
+      "0644",
+      "0648",
+      "0663",
+      "0664",
+      "0668",
+      "0623",
+      "0624",
+      "0628",
+      "0043",
+      "0044",
+      "0048",
+      "0063",
+      "0064",
+      "0068",
+      "0023",
+      "0024",
+      "0028",
+      "6443",
+      "6444",
+      "6448"
+    ],
+    "frases": []
+  },
   "LOTEP": {
     "palpites": [
       "3072",
@@ -152,5 +187,80 @@ const palpites = {
       "Resultado do Jogo do Bicho LOTEP - PB, 10:45\nDeu Dezena, Palpite 1619 no 7º Prêmio!",
       "Resultado do Jogo do Bicho LOTEP - PB, 10:45\nDeu Dezena, Palpite 1719 no 7º Prêmio!"
     ]
+  },
+  "PT RIO": {
+    "palpites": [
+      "2218",
+      "2210",
+      "2211",
+      "2248",
+      "2240",
+      "2241",
+      "2288",
+      "2280",
+      "2281",
+      "2618",
+      "2610",
+      "2611",
+      "2648",
+      "2640",
+      "2641",
+      "2688",
+      "2680",
+      "2681",
+      "2118",
+      "2110",
+      "2111",
+      "2148",
+      "2140",
+      "2141",
+      "2188",
+      "2180",
+      "2181",
+      "8218",
+      "8210",
+      "8211"
+    ],
+    "frases": [
+      "Resultado do Jogo do Bicho RJ, 09:20, PT, 1º ao 10º\nDeu Dezena, Palpite 0085 no 1º Prêmio!",
+      "Resultado do Jogo do Bicho RJ, 09:20, PT, 1º ao 10º\nDeu Dezena, Palpite 0185 no 1º Prêmio!",
+      "Resultado do Jogo do Bicho RJ, 09:20, PT, 1º ao 10º\nDeu Dezena, Palpite 0285 no 1º Prêmio!",
+      "Resultado do Jogo do Bicho RJ, 09:20, PT, 1º ao 10º\nDeu Dezena, Palpite 1085 no 1º Prêmio!"
+    ]
+  },
+  "PT SP": {
+    "palpites": [
+      "9942",
+      "9940",
+      "9946",
+      "9992",
+      "9990",
+      "9996",
+      "9972",
+      "9970",
+      "9976",
+      "9742",
+      "9740",
+      "9746",
+      "9792",
+      "9790",
+      "9796",
+      "9772",
+      "9770",
+      "9776",
+      "9042",
+      "9040",
+      "9046",
+      "9092",
+      "9090",
+      "9096",
+      "9072",
+      "9070",
+      "9076",
+      "0942",
+      "0940",
+      "0946"
+    ],
+    "frases": []
   }
 };
