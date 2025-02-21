23f8bdd55f1af5acab946c446a51053a9e303655
Rafael <157317208+RafaelDantasL@users.noreply.github.com>
2025-02-21 15:50:57 -0300

    Atualização automática dos palpites em 2025-02-21

--- palpites.js
+++ palpites.js
@@ -32,7 +32,19 @@ const palpites = {
       "4507",
       "4509"
     ],
-    "frases": []
+    "frases": [
+      "10h - MALUCA - Resultado do dia 21/02/2025 (Sexta-feira)\nDeu Dezena, Palpite 4128 no 2º Prêmio!",
+      "10h - MALUCA - Resultado do dia 21/02/2025 (Sexta-feira)\nDeu Dezena, Palpite 4528 no 2º Prêmio!",
+      "10h - MALUCA - Resultado do dia 21/02/2025 (Sexta-feira)\nDeu Dezena, Palpite 4028 no 2º Prêmio!",
+      "10h - MALUCA - Resultado do dia 21/02/2025 (Sexta-feira)\nDeu Dezena, Palpite 5128 no 2º Prêmio!",
+      "12h - MALUCA - BA - Resultado do dia 21/02/2025 (Sexta-feira)\nDeu Dezena, Palpite 5108 no 2º Prêmio!",
+      "12h - MALUCA - BA - Resultado do dia 21/02/2025 (Sexta-feira)\nDeu Milhar, Palpite 5508 no 2º Prêmio!",
+      "12h - MALUCA - BA - Resultado do dia 21/02/2025 (Sexta-feira)\nDeu Dezena, Palpite 5608 no 2º Prêmio!",
+      "12h - MALUCA - BA - Resultado do dia 21/02/2025 (Sexta-feira)\nDeu Dezena, Palpite 7108 no 2º Prêmio!",
+      "12h - MALUCA - BA - Resultado do dia 21/02/2025 (Sexta-feira)\nDeu Dezena, Palpite 5110 no 7º Prêmio!",
+      "12h - MALUCA - BA - Resultado do dia 21/02/2025 (Sexta-feira)\nDeu Dezena, Palpite 5510 no 7º Prêmio!",
+      "12h - MALUCA - BA - Resultado do dia 21/02/2025 (Sexta-feira)\nDeu Dezena, Palpite 5610 no 7º Prêmio!"
+    ]
   },
   "BAHIA": {
     "palpites": [
@@ -67,40 +79,44 @@ const palpites = {
       "9034",
       "9030"
     ],
-    "frases": []
+    "frases": [
+      "10h - BA - Resultado do dia 21/02/2025 (Sexta-feira)\nDeu Dezena, Palpite 0480 no 4º Prêmio!",
+      "10h - BA - Resultado do dia 21/02/2025 (Sexta-feira)\nDeu Dezena, Palpite 0580 no 4º Prêmio!",
+      "10h - BA - Resultado do dia 21/02/2025 (Sexta-feira)\nDeu Dezena, Palpite 0680 no 4º Prêmio!"
+    ]
   },
-  "PT SP": {
+  "LOTECE": {
     "palpites": [
-      "9838",
-      "9830",
-      "9831",
-      "9848",
-      "9840",
-      "9841",
-      "9858",
-      "9850",
-      "9851",
-      "9538",
-      "9530",
-      "9531",
-      "9548",
-      "9540",
-      "9541",
-      "9558",
-      "9550",
-      "9551",
-      "9938",
-      "9930",
-      "9931",
-      "9948",
-      "9940",
-      "9941",
-      "9958",
-      "9950",
-      "9951",
-      "8838",
-      "8830",
-      "8831"
+      "4114",
+      "4110",
+      "4111",
+      "4124",
+      "4120",
+      "4121",
+      "4144",
+      "4140",
+      "4141",
+      "4214",
+      "4210",
+      "4211",
+      "4224",
+      "4220",
+      "4221",
+      "4244",
+      "4240",
+      "4241",
+      "4414",
+      "4410",
+      "4411",
+      "4424",
+      "4420",
+      "4421",
+      "4444",
+      "4440",
+      "4441",
+      "8114",
+      "8110",
+      "8111"
     ],
     "frases": []
   }
