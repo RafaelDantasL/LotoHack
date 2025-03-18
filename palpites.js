36134b6786ee42f832a1244adce08363d3a33d55
Rafael <157317208+RafaelDantasL@users.noreply.github.com>
2025-03-18 12:28:08 -0300

    Atualização automática dos palpites em 2025-03-18

--- palpites.js
+++ palpites.js
@@ -246,6 +246,53 @@ const palpites = {
       "0024",
       "0029"
     ],
-    "frases": []
+    "frases": [
+      "Resultado do Jogo do Bicho RJ, 09:20, PT, 1º ao 10º\nDeu Dezena, Palpite 7166 no 1º Prêmio!",
+      "Resultado do Jogo do Bicho RJ, 09:20, PT, 1º ao 10º\nDeu Dezena, Palpite 7766 no 1º Prêmio!",
+      "Resultado do Jogo do Bicho RJ, 09:20, PT, 1º ao 10º\nDeu Dezena, Palpite 7666 no 1º Prêmio!",
+      "Resultado do Jogo do Bicho RJ, 09:20, PT, 1º ao 10º\nDeu Dezena, Palpite 7167 no 8º Prêmio!",
+      "Resultado do Jogo do Bicho RJ, 09:20, PT, 1º ao 10º\nDeu Dezena, Palpite 7767 no 8º Prêmio!",
+      "Resultado do Jogo do Bicho RJ, 09:20, PT, 1º ao 10º\nDeu Centena, Palpite 7667 no 8º Prêmio!",
+      "Resultado do Jogo do Bicho RJ, 09:20, PT, 1º ao 10º\nDeu Dezena, Palpite 7667 no 8º Prêmio!"
+    ]
+  },
+  "PT SP": {
+    "palpites": [
+      "5500",
+      "5505",
+      "5508",
+      "5510",
+      "5515",
+      "5518",
+      "5520",
+      "5525",
+      "5528",
+      "5100",
+      "5105",
+      "5108",
+      "5110",
+      "5115",
+      "5118",
+      "5120",
+      "5125",
+      "5128",
+      "5300",
+      "5305",
+      "5308",
+      "5310",
+      "5315",
+      "5318",
+      "5320",
+      "5325",
+      "5328",
+      "9500",
+      "9505",
+      "9508"
+    ],
+    "frases": [
+      "Resultado do Jogo do Bicho SP, 12:00 - PTSP\nDeu Dezena, Palpite 4030 no 4º Prêmio!",
+      "Resultado do Jogo do Bicho SP, 12:00 - PTSP\nDeu Dezena, Palpite 4230 no 4º Prêmio!",
+      "Resultado do Jogo do Bicho SP, 12:00 - PTSP\nDeu Dezena, Palpite 4630 no 4º Prêmio!"
+    ]
   }
 };
