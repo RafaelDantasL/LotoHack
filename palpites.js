fe24ddfbf8b1a3d1cd6cdea9477d07826e013699
Rafael <157317208+RafaelDantasL@users.noreply.github.com>
2025-04-05 15:21:18 -0300

    Update palpites.js

--- palpites.js
+++ palpites.js
@@ -281,4 +281,16 @@ const palpites = {
     ],
     "frases": []
   }
+
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-604824394"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-604824394');
+</script>
+
 };
