f310e21409f5fbc986f346ca665c3706530c9eee
Rafael <157317208+RafaelDantasL@users.noreply.github.com>
2025-06-09 10:33:06 -0300

    Atualização automática dos palpites em 2025-06-09

--- palpites.js
+++ palpites.js
@@ -1,4 +1,47 @@
 const palpites = {
+  "BAHIA MALUCA": {
+    "palpites": [
+      "0303",
+      "0300",
+      "0301",
+      "0353",
+      "0350",
+      "0351",
+      "0313",
+      "0310",
+      "0311",
+      "0503",
+      "0500",
+      "0501",
+      "0553",
+      "0550",
+      "0551",
+      "0513",
+      "0510",
+      "0511",
+      "0603",
+      "0600",
+      "0601",
+      "0653",
+      "0650",
+      "0651",
+      "0613",
+      "0610",
+      "0611",
+      "5303",
+      "5300",
+      "5301"
+    ],
+    "frases": [
+      "10h - MALUCA - Resultado do dia 09/06/2025 (Segunda-feira)\nDeu Centena, Palpite 1852 no 1º Prêmio!",
+      "10h - MALUCA - Resultado do dia 09/06/2025 (Segunda-feira)\nDeu Dezena, Palpite 1852 no 1º Prêmio!",
+      "10h - MALUCA - Resultado do dia 09/06/2025 (Segunda-feira)\nDeu Dezena, Palpite 1152 no 1º Prêmio!",
+      "10h - MALUCA - Resultado do dia 09/06/2025 (Segunda-feira)\nDeu Dezena, Palpite 1052 no 1º Prêmio!",
+      "10h - MALUCA - Resultado do dia 09/06/2025 (Segunda-feira)\nDeu Dezena, Palpite 1850 no 5º Prêmio!",
+      "10h - MALUCA - Resultado do dia 09/06/2025 (Segunda-feira)\nDeu Dezena, Palpite 1150 no 5º Prêmio!",
+      "10h - MALUCA - Resultado do dia 09/06/2025 (Segunda-feira)\nDeu Dezena, Palpite 1050 no 5º Prêmio!"
+    ]
+  },
   "BAHIA": {
     "palpites": [
       "0035",
@@ -31,6 +74,41 @@ const palpites = {
       "5035",
       "5030",
       "5031"
+    ],
+    "frases": []
+  },
+  "LOTECE": {
+    "palpites": [
+      "2349",
+      "2342",
+      "2344",
+      "2319",
+      "2312",
+      "2314",
+      "2339",
+      "2332",
+      "2334",
+      "2149",
+      "2142",
+      "2144",
+      "2119",
+      "2112",
+      "2114",
+      "2139",
+      "2132",
+      "2134",
+      "2249",
+      "2242",
+      "2244",
+      "2219",
+      "2212",
+      "2214",
+      "2239",
+      "2232",
+      "2234",
+      "8349",
+      "8342",
+      "8344"
     ],
     "frases": []
   },
@@ -191,6 +269,11 @@ const palpites = {
       "2360",
       "2367"
     ],
-    "frases": []
+    "frases": [
+      "Resultado do Jogo do Bicho SP, 08hs - PTSP, 1º ao 10º\nDeu Dezena, Palpite 5330 no 2º Prêmio!",
+      "Resultado do Jogo do Bicho SP, 08hs - PTSP, 1º ao 10º\nDeu Dezena, Palpite 5530 no 2º Prêmio!",
+      "Resultado do Jogo do Bicho SP, 08hs - PTSP, 1º ao 10º\nDeu Dezena, Palpite 5830 no 2º Prêmio!",
+      "Resultado do Jogo do Bicho SP, 08hs - PTSP, 1º ao 10º\nDeu Dezena, Palpite 0330 no 2º Prêmio!"
+    ]
   }
 };
