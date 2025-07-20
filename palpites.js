3f6faf4707dd3000c100efd920406f4aaf9a8150
Rafael <157317208+RafaelDantasL@users.noreply.github.com>
2025-07-20 17:35:07 -0300

    Atualização automática dos palpites em 2025-07-20

--- palpites.js
+++ palpites.js
@@ -298,5 +298,40 @@ const palpites = {
       "Resultado do Jogo do Bicho SP, 17hs - PTSP, 1º ao 10º\nDeu Dezena, Palpite 8232 no 3º Prêmio!",
       "Resultado do Jogo do Bicho SP, 17hs - PTSP, 1º ao 10º\nDeu Dezena, Palpite 8932 no 3º Prêmio!"
     ]
+  },
+  "LOTECE": {
+    "palpites": [
+      "3612",
+      "3616",
+      "3610",
+      "3642",
+      "3646",
+      "3640",
+      "3692",
+      "3696",
+      "3690",
+      "3712",
+      "3716",
+      "3710",
+      "3742",
+      "3746",
+      "3740",
+      "3792",
+      "3796",
+      "3790",
+      "3212",
+      "3216",
+      "3210",
+      "3242",
+      "3246",
+      "3240",
+      "3292",
+      "3296",
+      "3290",
+      "4612",
+      "4616",
+      "4610"
+    ],
+    "frases": []
   }
 };
