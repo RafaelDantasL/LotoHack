ea0a39195955cac46ff0d3bcd4a2d8d002826432
Rafael <157317208+RafaelDantasL@users.noreply.github.com>
2025-08-02 08:30:55 -0300

    Atualização automática dos palpites em 2025-08-02

--- palpites.js
+++ palpites.js
@@ -220,40 +220,5 @@ const palpites = {
       "Resultado do Jogo do Bicho SP, 08hs - PTSP, 1º ao 10º\nDeu Dezena, Palpite 2042 no 7º Prêmio!",
       "Resultado do Jogo do Bicho SP, 08hs - PTSP, 1º ao 10º\nDeu Dezena, Palpite 2142 no 7º Prêmio!"
     ]
-  },
-  "LOTECE": {
-    "palpites": [
-      "3382",
-      "3385",
-      "3386",
-      "3332",
-      "3335",
-      "3336",
-      "3342",
-      "3345",
-      "3346",
-      "3182",
-      "3185",
-      "3186",
-      "3132",
-      "3135",
-      "3136",
-      "3142",
-      "3145",
-      "3146",
-      "3882",
-      "3885",
-      "3886",
-      "3832",
-      "3835",
-      "3836",
-      "3842",
-      "3845",
-      "3846",
-      "5382",
-      "5385",
-      "5386"
-    ],
-    "frases": []
   }
 };
