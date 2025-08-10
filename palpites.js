31402e1c15801dce63a1afb545c6d73f7cebc9c6
Rafael <157317208+RafaelDantasL@users.noreply.github.com>
2025-08-10 20:35:56 -0300

    Atualização automática dos palpites em 2025-08-10

--- palpites.js
+++ palpites.js
@@ -296,40 +296,5 @@ const palpites = {
       "Resultado do Jogo do Bicho SP, 20hs - PTN SP, 1º ao 10º\nDeu Centena, Palpite 0013 no 6º Prêmio!",
       "Resultado do Jogo do Bicho SP, 20hs - PTN SP, 1º ao 10º\nDeu Dezena, Palpite 0013 no 6º Prêmio!"
     ]
-  },
-  "LOTECE": {
-    "palpites": [
-      "5390",
-      "5397",
-      "5391",
-      "5300",
-      "5307",
-      "5301",
-      "5310",
-      "5317",
-      "5311",
-      "5690",
-      "5697",
-      "5691",
-      "5600",
-      "5607",
-      "5601",
-      "5610",
-      "5617",
-      "5611",
-      "5990",
-      "5997",
-      "5991",
-      "5900",
-      "5907",
-      "5901",
-      "5910",
-      "5917",
-      "5911",
-      "2390",
-      "2397",
-      "2391"
-    ],
-    "frases": []
   }
 };
