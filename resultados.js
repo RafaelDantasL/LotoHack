41dc8a286c4da0dad3bef89be649060925420ae4
Rafael <157317208+RafaelDantasL@users.noreply.github.com>
2026-04-16 11:45:48 -0300

    Atualização automática dos resultados em 2026-04-16

--- resultados.js
+++ resultados.js
@@ -714,6 +714,59 @@ const resultado = {
           ""
         ]
       ]
+    },
+    {
+      "titulo": "Resultado do Jogo do Bicho BOA SORTE - GOIÁS, 11h",
+      "conteudo": [
+        [
+          "Prêmio",
+          "Milhar",
+          "Grupo",
+          "Bicho"
+        ],
+        [
+          "1º",
+          "9052",
+          "13",
+          "Galo"
+        ],
+        [
+          "2º",
+          "0092",
+          "23",
+          "Urso"
+        ],
+        [
+          "3º",
+          "7039",
+          "10",
+          "Coelho"
+        ],
+        [
+          "4º",
+          "3688",
+          "22",
+          "Tigre"
+        ],
+        [
+          "5º",
+          "2448",
+          "12",
+          "Elefante"
+        ],
+        [
+          "6º [soma]",
+          "2319",
+          "",
+          ""
+        ],
+        [
+          "7º [mult]",
+          "832",
+          "",
+          ""
+        ]
+      ]
     }
   ],
   "LOTEP": [
